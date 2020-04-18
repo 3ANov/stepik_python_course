4d0be146bd618a94269f3a1d04646b07c073fa65
3ANov <3anovdev@gmail.com>
2020-04-18 15:19:07 +0300

    Добавил решение

---
+++ Стандартные средства языка Python/Итераторы и генераторы/task2.3.2.py
@@ -0,0 +1,25 @@
+import itertools
+import math
+
+
+def primes():
+    i = 2
+    while True:
+        is_prime = True
+        divisor = 2
+        while divisor ** 2 <= i:
+            if i % divisor == 0:
+                is_prime = False # non-trivial divisor
+                break
+
+            divisor += 1
+
+        if is_prime:
+            yield i
+
+        i += 1
+
+
+
+print(list(itertools.takewhile(lambda x : x <= 31, primes())))
+# [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
