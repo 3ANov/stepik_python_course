ac3beba8b4e0b2c7b8e88dd5b4ee3c07e267b916
3ANov <3anovdev@gmail.com>
2020-04-27 21:54:35 +0300

    Дундук - забыл отладку убрать )

--- Применение Python: анализ текста/Обзорно об интернете: http-запросы, html-страницы и requests/task3.4.2.py
+++ Применение Python: анализ текста/Обзорно об интернете: http-запросы, html-страницы и requests/task3.4.2.py
@@ -2,19 +2,17 @@
 import requests
 import re
 
-start = 'http://pastebin.com/raw/7543p0ns'
-#href=[\'|\"]((\w*\:\/\/)?)(\b([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}\b)
+
+start = input()
 link_template = r'<a.*href=[\'|\"]((\w*\:\/\/)?)(\b([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}\b)'
-res = requests.get(start)
-domain_name = []
+res = requests.get(start).text
 
-#print(re.findall(link_template, res.text))
-result = set()
-for group in re.findall(link_template, res.text):
-    #print(group[2])
-    result.add(group[2])
 
-result = list(result)
+result_set = set()
+for group in re.findall(link_template, res):
+    result_set.add(group[2])
+
+result = list(result_set)
 result.sort()
 for line in result:
     print(line)
