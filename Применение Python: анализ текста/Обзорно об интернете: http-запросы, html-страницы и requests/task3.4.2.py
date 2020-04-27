6c658af2711773817bf66aa06e0b8db86f803e26
3ANov <3anovdev@gmail.com>
2020-04-27 21:22:08 +0300

    На верном пути

--- Применение Python: анализ текста/Обзорно об интернете: http-запросы, html-страницы и requests/task3.4.2.py
+++ Применение Python: анализ текста/Обзорно об интернете: http-запросы, html-страницы и requests/task3.4.2.py
@@ -1,11 +1,20 @@
+# put your python code here
 import requests
 import re
-from urllib.parse import urlparse
 
+start = 'http://pastebin.com/raw/7543p0ns'
+#href=[\'|\"]((\w*\:\/\/)?)(\b([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}\b)
+link_template = r'href=[\'|\"]((\w*\:\/\/)?)(\b([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}\b)'
+res = requests.get(start)
+domain_name = []
 
-with open('input.txt', 'r') as inp:
-    for input_line in inp:
-        print(input_line.rstrip().split('\''))
+#print(re.findall(link_template, res.text))
+result = set()
+for group in re.findall(link_template, res.text):
+    #print(group[2])
+    result.add(group[2])
 
-
-
+result = list(result)
+result.sort()
+for line in result:
+    print(line)
